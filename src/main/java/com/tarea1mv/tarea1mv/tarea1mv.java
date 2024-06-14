d5a536a859ad16de4546eb791b3ff8a9bfe066ef
kristel sofía Sancho herrera <Sofiasancho0219@gmail.com>
2024-06-14 03:26:41 -0600

    Tarea 1, SEMANA 3

---
+++ src/main/java/com/tarea1mv/tarea1mv/tarea1mv.java
@@ -0,0 +1,13 @@
+package com.tarea1mv.tarea1mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tarea1mv {
+
+	public static void main(String[] args) {
+		SpringApplication.run(tarea1mv.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea1mv/tarea1mv/tarea1mvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea1mv.tarea1mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class tarea1mvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
